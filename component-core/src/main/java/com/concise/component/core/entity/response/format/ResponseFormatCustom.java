15ce87807e1e72097507485c5ef2e4ce13278ee1
shenguangyang <shenguangyang@foxmail.com>
2021-12-23 20:24:47 +0800

    扫描兼容多种响应格式handler的子类

--- component-core/src/main/java/com/concise/component/core/entity/response/format/ResponseFormatCustom.java
+++ component-core/src/main/java/com/concise/component/core/entity/response/format/ResponseFormatCustom.java
@@ -1,9 +1,12 @@
 package com.concise.component.core.entity.response.format;
 
+import org.springframework.stereotype.Component;
+
 /**
  * @author shenguangyang
  * @date 2021/7/13 1:22
  */
+@Component
 public class ResponseFormatCustom extends ResponseFormatAbstract {
 
     @Override
