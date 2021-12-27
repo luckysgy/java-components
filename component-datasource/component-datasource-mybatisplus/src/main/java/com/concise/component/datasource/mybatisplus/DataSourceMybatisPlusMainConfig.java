3c0f8cc4a21311174ec29af423e4ec4239e39151
shenguangyang <shenguangyang@foxmail.com>
2021-12-27 20:52:10 +0800

    删除不存在的依赖

--- component-datasource/component-datasource-mybatisplus/src/main/java/com/concise/component/datasource/mybatisplus/DataSourceMybatisPlusMainConfig.java
+++ component-datasource/component-datasource-mybatisplus/src/main/java/com/concise/component/datasource/mybatisplus/DataSourceMybatisPlusMainConfig.java
@@ -1,6 +1,5 @@
 package com.concise.component.datasource.mybatisplus;
 
-import com.concise.component.datasource.mybatisplus.register.EnableMybatisPlus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.ComponentScan;
