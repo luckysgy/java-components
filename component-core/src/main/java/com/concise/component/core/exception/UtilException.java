ab84cdcff3e0bdb49f1ee06c9171093377fd8221
shenguangyang <shenguangyang@foxmail.com>
2022-06-26 15:42:00 +0800

    refactor: 去掉线程池配置

--- component-core/src/main/java/com/concise/component/core/exception/UtilException.java
+++ component-core/src/main/java/com/concise/component/core/exception/UtilException.java
@@ -3,7 +3,7 @@ package com.concise.component.core.exception;
 /**
  * 工具类异常
  * 
- * @author ruoyi
+ * @author shenguangyang
  */
 public class UtilException extends RuntimeException {
     private static final long serialVersionUID = 8247610319171014183L;
